62fcccc0fac6abf7a05efb553d04b1a02e448220
HSN <hasanlock@gmail.com>
2025-03-26 23:05:05 +0600

    feat: add test code for pipeline

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,7 +1,8 @@
 """Tests for Pipeline"""
 
+import icecream
 import pytest
-from hence.init import Pipeline
+from hence import Pipeline, PipelineContext
 
 
 class TestPipelineParameter:
@@ -21,3 +22,67 @@ class TestPipelineParameter:
 
         assert isinstance(p.parameter(a_param=21), Pipeline)
         assert p.context.parameters["a_param"] == 21
+
+    @staticmethod
+    def test_fails_for_unregister_function():
+        pipeline = Pipeline()
+
+        @pipeline.add_task(pass_ctx=True)
+        def function_1(ctx, a: str):
+            return function_1.__name__
+
+        with pytest.raises(KeyError):
+            pipeline.parameter(function_2={"a": "String"})
+
+    @staticmethod
+    def test_pass_for_register_function():
+        pipeline = Pipeline()
+
+        @pipeline.add_task(pass_ctx=True)
+        def function_1(ctx, a: str):
+            return function_1.__name__
+
+        pipeline.parameter(function_1={"a": "String"})
+
+        assert pipeline.context.parameters["function_1"] == {"a": "String"}
+
+
+class TestPipelineAddTask:
+    @staticmethod
+    def test_fail_for_no_ctx_var():
+        pipeline = Pipeline()
+
+        with pytest.raises(AttributeError):
+
+            @pipeline.add_task(pass_ctx=True)
+            def function_1():
+                return function_1.__name__
+
+    @staticmethod
+    def test_fail_for_misleading_type():
+        pipeline = Pipeline()
+
+        with pytest.raises(AttributeError):
+
+            @pipeline.add_task(pass_ctx=True)
+            def function_1(ctx: int, a: str):
+                return function_1.__name__
+
+    @staticmethod
+    def test_pass_for_correct_context_type():
+        pipeline = Pipeline()
+
+        @pipeline.add_task(pass_ctx=True)
+        def function_1(ctx: PipelineContext, a: str):
+            return function_1.__name__
+
+    @staticmethod
+    def test_pass_for_no_context_type():
+        pipeline = Pipeline()
+
+        @pipeline.add_task(pass_ctx=True)
+        def function_1(ctx, a: str):
+            return function_1.__name__
+
+class TestPipelineRun:
+    
